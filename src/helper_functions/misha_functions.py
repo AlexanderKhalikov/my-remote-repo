d9f0ceb3b406e9f20c5f53e97097b30062f5d884
AlexanderKhalikov <sasha.khalikov@gmail.com>
2023-01-16 19:27:11 +1100

    Новые тесты

--- src/helper_functions/misha_functions.py
+++ src/helper_functions/misha_functions.py
@@ -1,6 +1,20 @@
+
+def my_new_function(
+        a: int = 0,
+        b: float = 0.0,
+        c: str = ''
+) -> list:
+    return [a, b, c]
+
+
 def my_sum(a: int, b: int) -> int:
     return a + b
 
 
 def my_multiply(a: int, b: int) -> int:
     return a * b
+
+
+if __name__ == '__main__':
+
+    print(type(my_new_function(a=4, b=4.5, c='qwerty')))
--- tests/test_misha_functions.py
+++ tests/test_misha_functions.py
@@ -2,20 +2,32 @@ from src.helper_functions import misha_functions
 import pytest
 
 
-@pytest.mark.parametrize("a, b, expected", [
-    (4, 6, 10),
-    (5, 7, 12),
-    (2, 2, 4),
-    (3, 7, 10)
+# @pytest.mark.parametrize("a, b, expected", [
+#     (4, 6, 10),
+#     (5, 7, 12),
+#     (2, 2, 4),
+#     (3, 7, 10)
+# ])
+# def test_my_sum(a, b, expected):
+#     assert misha_functions.my_sum(a, b) == expected
+#
+#
+# @pytest.mark.parametrize("a, b, expected", [
+#     (4, 6, 24),
+#     (5, 7, 35),
+# ])
+# def test_my_multiply(a, b, expected):
+#     assert misha_functions.my_multiply(a, b) == expected
+
+
+def test_my_multiply_is_list():
+    assert isinstance(misha_functions.my_new_function(), list)
+
+
+@pytest.mark.parametrize("a, b, c", [
+    (1, 1.5, 'qwerty'),
+    (2, 2.5, 'asdfgh'),
+    (7, 3.8, 'laksjdfnpqweiojf')
 ])
-def test_my_sum(a, b, expected):
-    assert misha_functions.my_sum(a, b) == expected
-
-
-@pytest.mark.parametrize("a, b, expected", [
-    (4, 6, 24),
-    (5, 7, 35),
-])
-def test_my_multiply(a, b, expected):
-    assert misha_functions.my_multiply(a, b) == expected
-
+def test_my_multiply_compare(a, b, c):
+    assert misha_functions.my_new_function(a, b, c) == [a, b, c]
